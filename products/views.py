6213789e39b8fe151458ab473d25d0724b8b7551
valerie <ooakhu@gmail.com>
2020-10-23 21:35:12 +0100

    first commit

---
+++ products/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from products.models import Product
+# Create your views here.
+
+
+class Home(ListView):
+    model = Product
+    template_name = 'products/home.html'
